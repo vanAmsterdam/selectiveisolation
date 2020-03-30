2ecab6c43861f024a76d50581458d9cb2a62f00f
vanAmsterdam <woutervanamsterdam@gmail.com>
2020-03-30 21:36:34 +0200

    FIX deathrates and add math

--- code/01_mergeandmodel.R
+++ code/01_mergeandmodel.R
@@ -60,13 +60,13 @@ agem %>%
   labs(y='Incindence rate')
 
 agem %>%
-  ggplot(aes(x=agegrp_min,y=normalized_ir,group=variable,col=variable)) + 
+  ggplot(aes(x=agegrp_min,y=ir_norm,group=variable,col=variable)) + 
   geom_line() + theme_minimal() + 
   ggtitle('Relative occurences',  'of known infection, hospitalization and deaths per age group') +
   labs(y='Proportion of total per variable')
 
 ## make some scenarios
-marginaldeathrates <- seq(1.0, 3.5, by=0.5)
+marginaldeathrates <- seq(0.005, 0.035, by=0.005)
 herdimmunityprops  <- seq(0.6, 0.8, by=0.05)
 freeages           <- c(65, 130)
                           
@@ -93,6 +93,20 @@ settings[, herd_needed:=herdimmunityprop*demsmf[, sum(Bevolking)]]
 settings[, herd_avail:=demsmf[agegrp_max<freeage, max(Bevolking_cum)], by = 'setting_idx']
 settings[, herd_prop:=herd_needed/herd_avail]
 
+## using these equations:
+
+# R_{marg} = \sum_i{p_i*R_i}
+# Where $p_i$ the proportion of the population in age group $i$, 
+# $R_i$ the death rate in that age group
+# Introduce a single factor R such that $R_i = R * rr_i$, and that $\sum_i{rr_i}=1$
+# Then
+# R_{marg} = \sum_i{p_i*R*rr_i} = R * \sum_i{p_i*rr_i}
+# So now we have that
+# R = \frac{R_{marg}}{\sum_{p_i*rr_i}}
+# Calculate $\sum_{p_i*rr_i} := ir_mu$
+ir_mu = agem[variable=='Overleden'][demsmf, sum(ir_norm*i.Bevolking_prop), on='Leeftijdsgroep']
+settings[, Rfactor:=marginaldeathrate / ir_mu]
+
 #### create outer tables for each setting
 agem[, dumkey:=1]
 dfs <- agem[variable=='Overleden'][settings, on='dumkey', allow.cartesian=T]
@@ -101,34 +115,80 @@ dfs <- agem[variable=='Overleden'][settings, on='dumkey', allow.cartesian=T]
 #### calculate number infected
 dfs[, inherd:=agegrp_max < freeage]
 dfs[demsmf, Bevolking:=i.Bevolking, on='Leeftijdsgroep']
+dfs[demsmf, Bevolking_prop:=i.Bevolking_prop, on='Leeftijdsgroep']
 dfs[, num_infected:=herd_prop*Bevolking*as.numeric(agegrp_max<freeage)]
 
 
 ##### calculate total number of deaths
-dfs[, deathrate:=marginaldeathrate*ir_norm]
-dfs[demsmf, deaths:=deathrate*num_infected, on='Leeftijdsgroep']
+dfs[, deathrate:=Rfactor*ir_norm]
+###### make sure the marginal death rates are ok
+drs <- dfs[, list(obs_marginaldeathrate=sum(Bevolking_prop*deathrate)), by=c('setting_idx', 'marginaldeathrate')]
+stopifnot(drs[, all.equal(obs_marginaldeathrate, marginaldeathrate)])
+dfs[, deaths:=deathrate*num_infected]
+
+### define a baseline (freeage==130)
+baseline <- dfs[freeage==130]
+dfs[baseline, deathdifference:=deaths - i.deaths, 
+    on=c('marginaldeathrate', 'herdimmunityprop', 'Leeftijdsgroep')]
+dfs[, excessdeaths:=ifelse(deathdifference>0, deathdifference, 0)]
+dfs[, preventeddeaths:=ifelse(deathdifference<0, -deathdifference, 0)]
+dfs[, totaldeaths:=sum(deaths), by='setting_idx']
 
 ## tables
-totaldeaths <- dfs[, list(deaths=sum(deaths), num_infected=sum(num_infected)),
+totaldeaths <- dfs[, list(deaths=sum(deaths), 
+                          num_infected=sum(num_infected)),
     by=c('marginaldeathrate', 'herdimmunityprop', 'freeage', 'setting_idx')]
 totaldeaths[order(deaths)]
 dcast(totaldeaths, herdimmunityprop~freeage+marginaldeathrate, value.var='deaths')
 
 livessaved <- dcast(totaldeaths[freeage==max(freeage)], herdimmunityprop~marginaldeathrate, value.var='deaths') - 
               dcast(totaldeaths[freeage==min(freeage)], herdimmunityprop~marginaldeathrate, value.var='deaths')
-  
+livessaved 
 
 ## plots
+### deathrate
+dfs %>%
+  .[!duplicated(paste0(marginaldeathrate, Leeftijdsgroep))] %>%
+  mutate(marginaldeathrate=factor(marginaldeathrate)) %>%
+  ggplot(aes(x=agegrp_min, y=deathrate, grp=marginaldeathrate,
+             col=marginaldeathrate)) + 
+  geom_line() + 
+  theme_minimal() + 
+  ggtitle('Age-specific death rate', 'per marginal death rate')
+
 dfs %>% 
-  mutate(
-    freeage=factor(freeage)
-    ) %>%
+  mutate(freeage=factor(freeage)) %>%
   ggplot(aes(x=agegrp_min, y=deaths, group=setting_idx, col=freeage)) + 
   geom_line() + 
   facet_grid(herdimmunityprop~marginaldeathrate) + 
-  theme_minimal()
+  theme_minimal() + 
+  ggtitle('Total number of deaths', 'Per marginal deathrate and herd immunity proportion, and isolation type (freeage)')
+
+dfs %>% 
+  .[freeage<130] %>%
+  .[excessdeaths>=0,] %>%
+  mutate(freeage=factor(freeage)) %>%
+  ggplot(aes(x=agegrp_min, y=excessdeaths, group=setting_idx)) + 
+  geom_line() + 
+  facet_grid(herdimmunityprop~marginaldeathrate) + 
+  theme_minimal() + 
+  ggtitle('Excess deaths', 'Deaths that would not have occured in the non-selective isolation method')
+
+dfs %>%
+  .[freeage<130] %>%
+  ggplot(aes(x=totaldeaths, y=excessdeaths, grp=Leeftijdsgroep)) + 
+  geom_line() + 
+  facet_wrap(~Leeftijdsgroep) + 
+  theme_minimal() + 
+  ggtitle('Excess deaths', 'Versus total deaths')
   
-totaldeaths %>%
-  ggplot(aes(x=herdimmunityprop, y=marginaldeathrate)) + 
-  geom_tile(aes(fill=deaths)) + 
-  facet_wrap(~freeage)
+dfs %>%
+  .[freeage<130] %>%
+  .[, preventeddeaths:=sum(preventeddeaths), by='setting_idx'] %>%
+  ggplot(aes(x=preventeddeaths, y=excessdeaths, grp=Leeftijdsgroep)) + 
+  geom_line() + 
+  facet_wrap(~Leeftijdsgroep) + 
+  theme_minimal() + 
+  ggtitle('Excess deaths', 'Versus preventeddeaths')
+  
+
